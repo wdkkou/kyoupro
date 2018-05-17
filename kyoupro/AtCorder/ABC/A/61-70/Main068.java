8e9e63fa9aa5aa86fc89038d37f5e4b22c5cae61
wdkkou <koukouki7547@gmail.com>
2018-05-17 22:46:24 +0900

    delete file

--- kyoupro/AtCorder/ABC/A/61-70/Main068.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Main068{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        System.out.println("ABC" + N );
-    }
-}
