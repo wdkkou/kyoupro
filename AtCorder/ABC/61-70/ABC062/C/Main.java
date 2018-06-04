1d8d6bd2fda923340caf6ea49f78dea0d92640b2
wdkkou <koukouki7547@gmail.com>
2018-06-04 17:57:20 +0900

    ABC062

---
+++ AtCorder/ABC/61-70/ABC062/C/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int w = sc.nextInt();
+    }
+}
