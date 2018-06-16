d07dac4e247c3eaf60733a9ee889da7f2c554e89
wdkkou <koukouki7547@gmail.com>
2018-06-17 08:18:46 +0900

    ABC100/D/

--- AtCorder/ABC/91-100/ABC100/D/Main.java
+++ AtCorder/ABC/91-100/ABC100/D/Main.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -7,18 +10,39 @@ public class Main{
         List<Long> x = new ArrayList<>();
         List<Long> y = new ArrayList<>();
         List<Long> z = new ArrayList<>();
-        for(int i = 1;i <= n;i++){
-            x.add(sc.nextInt());
-            y.add(sc.nextInt());
-            z.add(sc.nextInt());
+        for(int i = 0;i < n;i++){
+            x.add(sc.nextLong());
+            y.add(sc.nextLong());
+            z.add(sc.nextLong());
         }
-        long dp[] = new long[];
-        dp[0] = 0;
-        for(int i = 1;i <= n;i++){
-            for(int j = 1;j <= n;j++){
-                dp[i] = Math.max(dp[i-1],x.get(j)+y.get(j)+z.get(j));
+        List<Long> w = new ArrayList<>();;
+        w.add(-1L);
+        w.add(1L);
+        long max = 0;
+        for(Long it1 : w){
+            for(Long it2 : w){
+                for(Long it3 : w){
+                    List<Long> sum = new ArrayList<>();;
+                    for(int i = 0;i < n;i++){
+                        long total = it1*x.get(i) + it2*y.get(i) + it3*z.get(i);
+                        sum.add(total);
+                    }
+                    //System.out.printf("w1 = %d,w2 = %d,w3 = %d\n",it1,it2,it3);
+                    //System.out.println("sum = " + sum);
+                    //System.out.println("sum = " + sum);
+                    max = Math.max(max,solve(sum,m));
+                }
             }
         }
-        System.out.println(dp[n]);
+        System.out.println(max);
+    }
+    public static long solve(List<Long> sum,int k){
+        Collections.sort(sum,Collections.reverseOrder());
+        long max = 0;
+        for(int i = 0;i < k;i++){
+           max += sum.get(i);
+        }
+        //System.out.println(max);
+        return max;
     }
 }
