482ad092386ef00d5414958d5f72c91964f8e5be
wdkkou <koukouki7547@gmail.com>
2018-07-20 15:23:47 +0900

    ABC077/C/

--- AtCorder/ABC/71-80/ABC077/C/Main.java
+++ AtCorder/ABC/71-80/ABC077/C/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -17,5 +18,46 @@ public class Main{
         for(int i = 0;i < n;i++){
             c.add(sc.nextInt());
         }
+        Collections.sort(a);
+        Collections.sort(c);
+        long count = 0;
+        for(int i= 0;i < n;i++){
+            long k = lower_bound(n,a,b.get(i));
+            long j = n - upper_bound(n,c,b.get(i));
+            count += k*j;
+        }
+        System.out.println(count);
+    }
+    public static long lower_bound(int n,List x,int k){
+        int lb = -1;
+        int ub = n;
+
+        while(ub - lb > 1){
+            int mid = (ub + lb) / 2;
+            int m = (int) x.get(mid);
+            if(m >= k){
+                ub = mid;
+            }
+            else{
+                lb = mid;
+            }
+        }
+        return ub;
+    }
+    public static long upper_bound(int n,List x,int k){
+        int lb = -1;
+        int ub = n;
+
+        while(ub - lb > 1){
+            int mid = (ub + lb) / 2;
+            int m = (int) x.get(mid);
+            if(m <= k){
+                lb = mid;
+            }
+            else{
+                ub = mid;
+            }
+        }
+        return ub;
     }
 }
