23e29f056bcbfae6935de3fb73033dea4589f239
wdkkou <koukouki7547@gmail.com>
2018-07-23 17:33:27 +0900

    ABC042/

---
+++ AtCorder/ABC/41-50/ABC042/D/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int w = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[][] dot = new int[h][w];
+        
+        for(int i = 0;i < h;i++){
+            for(int j = 0;j < w;j++){
+            }
+        }
+    }
+    public static long solve(){
+        for(int i = 0; i <= h+w-2;i++){
+        }
+    }
+}
--- AtCorder/ABC/81-90/ABC085/C/Main.java
+++ AtCorder/ABC/81-90/ABC085/C/Main.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
+        int n = sc.nextInt();
+        int total = sc.nextInt();
+        for(int x = 0;x <= n;x++){
+            for(int y = 0;y <= n;y++){
+                int z = n - x - y;
+                if(z < 0){
+                    continue;
+                }
+                if(10000*x + 5000*y + 1000*z == total){
+                    System.out.printf("%d %d %d\n",x,y,z);
+                    return;
+                }
+            }
+        }
+        System.out.printf("%d %d %d\n",-1,-1,-1);
     }
 }
