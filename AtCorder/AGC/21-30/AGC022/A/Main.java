f3935aa249a9a066327d38a23f53360b3de89f11
wdkkou <koukouki7547@gmail.com>
2018-08-07 13:44:29 +0900

    AGC022/

--- AtCorder/AGC/21-30/AGC022/A/Main.java
+++ AtCorder/AGC/21-30/AGC022/A/Main.java
@@ -7,29 +7,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        Set<Character> str = new TreeSet<>();
-        for (int i = 0; i < s.length(); i++) {
-            str.add(s.charAt(i));
+        String atoz = "abcdefghijklmnopqrstuvwxyz";
+        for(String str : s.split("")){
+            atoz = atoz.replace(str,"");
         }
         if(s.length() != 26){
-            Iterator it = str.iterator();
-            char ch = 'a';
-            for (int i = 0; i < 26; i++) {
-                char x = (char) it.next();
-                if (x != ch) {
-                    System.out.println(s + ch);
-                    break;
-                }
-                ch++;
-            }
+            System.out.println(s + atoz.charAt(0));
             return;
         }
         int max = -1;
         char maxc = ' ';
         for(int i = 25;i >= 0;i--){
-            char tmp = str.charAt(i);
+            char tmp = s.charAt(i);
             for(int j = i-1;j >= 0;j--){
-                if(str.charAt(j) < tmp){
+                if(s.charAt(j) < tmp){
                     if(max < j){
                         max = j;
                         maxc = tmp;
@@ -37,5 +28,10 @@ public class Main {
                 }
             }
         }
+        if(max != -1){
+            System.out.println(s.substring(0,max) + maxc);
+            return;
+        }
+        System.out.println(-1);
     }
 }
