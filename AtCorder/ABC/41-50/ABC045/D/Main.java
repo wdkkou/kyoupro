f98cdf4e24478bbb9b056a44ca3a978e2eddc5e4
wdkkou <koukouki7547@gmail.com>
2018-11-06 01:10:23 +0900

    ABC045

--- AtCorder/ABC/41-50/ABC045/D/Main.java
+++ AtCorder/ABC/41-50/ABC045/D/Main.java
@@ -10,10 +10,34 @@ public class Main {
             Point p = new Point(sc.nextInt(), sc.nextInt());
             points.add(p);
         }
-        Map<Point, Long> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            for(int x = ){
+        Map<Point, Integer> map = new HashMap<>();
+        for (Point p : points) {
+            // System.out.printf("p : %s %n", p);
+            for (int x = 0; x < 3; x++) {
+                for (int y = 0; y < 3; y++) {
+                    int nx = p.row - x;
+                    int ny = p.line - y;
+                    if (nx <= 0 || ny <= 0 || nx > w - 2 || ny > h - 2) {
+                        continue;
+                    }
+                    Point point = new Point(nx, ny);
+                    if (map.containsKey(point)) {
+                        map.put(point, map.get(point) + 1);
+                    } else {
+                        map.put(point, 1);
+                    }
+                }
             }
+        }
+
+        long[] ans = new long[10];
+        ans[0] = (long) (w - 2) * (h - 2);
+        map.forEach((c, cnt) -> {
+            ans[cnt]++;
+            ans[0]--;
+        });
+        for (int i = 0; i < 10; i++) {
+            System.out.println(ans[i]);
         }
     }
 }
@@ -33,5 +57,11 @@ class Point {
         }
         Point p = (Point) o;
         return this.line == p.line && this.row == p.row;
+    }
+    public int hashCode() {
+        return Objects.hash(line, row);
+    }
+    public String toString() {
+        return String.format("line = %s,row = %s", line, row);
     }
 }
