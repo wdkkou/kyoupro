e1c9cb4923796097df1aa079487708f4642373fc
wdkkou <koukouki7547@gmail.com>
2018-11-15 00:28:07 +0900

    ABC113/C

--- AtCorder/ABC/111-120/ABC113/C/Main.java
+++ AtCorder/ABC/111-120/ABC113/C/Main.java
@@ -1,32 +1,61 @@
 import java.util.Scanner;
 import java.util.*;
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        List<Integer> yearList = new ArrayList<>();
-        List<Integer> indexList = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] nm = reader.readLine().split(" ");
+        int n = Integer.valueOf(nm[0]);
+        int m = Integer.valueOf(nm[1]);
+
+        Map<Integer, List<Pair>> map = new HashMap<>();
         for (int i = 0; i < m; i++) {
-            int index = sc.nextInt();
-            int year = sc.nextInt();
-            yearList.add(year);
-            indexList.add(index);
-            if (!map.containsKey(index)) {
-                List<Integer> list = new ArrayList<>();
-                map.put(index, list);
+            String[] py = reader.readLine().split(" ");
+            int pre = Integer.valueOf(py[0]);
+            int year = Integer.valueOf(py[1]);
+            if (!map.containsKey(pre)) {
+                List<Pair> pairs = new ArrayList<>();
+                map.put(pre, pairs);
             }
-            map.get(index).add(year);
+            Pair p = new Pair(i, year);
+            map.get(pre).add(p);
+        }
+        for (Map.Entry<Integer, List<Pair>> entry : map.entrySet()) {
+            Collections.sort(entry.getValue(), new PairComparator());
+        }
+        String[] ans = new String[m];
+        for (Map.Entry<Integer, List<Pair>> entry : map.entrySet()) {
+            int order = 0;
+            for (Pair p : entry.getValue()) {
+                order++;
+                ans[p.city] = String.format("%06d%06d", entry.getKey(), order);
+            }
         }
 
-        Collections.sort(yearList);
-
-        for (int i = 1; i <= n; i++) {
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        for (String s : ans) {
+            writer.println(s);
         }
-
-        for (int i = 0; i < m; i++) {
-            System.out.printf("%06d%06d%n", indexList.get(i), y);
-        }
+        writer.flush();
+    }
+}
+class Pair {
+    int city;
+    int year;
+    public Pair(int city, int year) {
+        this.city = city;
+        this.year = year;
+    }
+    public int compareTo(Pair p) {
+        return this.year - p.year;
+    }
+    public String toString() {
+        return String.format("city : %d year: %d %n", this.city, this.year);
+    }
+}
+class PairComparator implements Comparator<Pair> {
+    public int compare(Pair p1, Pair p2) {
+        return p1.year - p2.year;
     }
 }
