0e6148b9a7168ee3a2e3fd9e27ceb710dd155fbd
wdkkou <koukouki7547@gmail.com>
2018-12-15 04:20:48 +0900

    ABC034 ABC061/D

--- AtCorder/ABC/61-70/ABC061/D/Main.java
+++ AtCorder/ABC/61-70/ABC061/D/Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Main {
-    static long INF = Long.MAX_VALUE;
+    static long INF = 100000000000000L;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -12,58 +12,64 @@ public class Main {
             long cost = -sc.nextLong();
             edges.add(new Edge(from, to, cost));
         }
-        if (negativeLoop(edges, 1, n, m)) {
+        long[] d = new long[100000];
+        d = bellmanFord(d, edges, 1, n, m);
+        if (negativeLoop(d, edges, 1, n, m)) {
             System.out.println("inf");
             return;
         }
-        bellmanFord(edges, 1, n, m);
         // System.out.println(Arrays.toString(d));
         System.out.println(-d[n]);
     }
 
-    static long[] d = new long[100000];
-    public static void bellmanFord(List<Edge> es, int s, int vl, int el) {
+    public static long[] bellmanFord(long[] d, List<Edge> es, int s, int vl, int el) {
         for (int i = 0; i <= vl; i++) {
             d[i] = INF;
         }
         d[s] = 0;
-        while (true) {
-            boolean update = false;
+        for (int j = 0; j < vl; j++) {
             for (int i = 0; i < el; i++) {
                 Edge e = es.get(i);
                 // System.out.println(e);
-                // System.out.printf(
-                //    "d[%d] = %d d[%d] = %d e.cost = %d%n", e.to, d[e.to], e.from,
-                //    d[e.from],e.cost);
+                // System.out.printf("d[%d] = %d d[%d] = %d e.cost = %d%n", e.to, d[e.to], e.from,
+                //     d[e.from], e.cost);
                 if (d[e.from] != INF && d[e.to] > d[e.from] + e.cost) {
                     d[e.to] = d[e.from] + e.cost;
                     //   System.out.printf("d[%d] = %d %n", e.to, d[e.to]);
-                    update = true;
                 }
             }
-            if (!update) {
-                break;
-            }
         }
+        return d;
     }
 
-    public static boolean negativeLoop(List<Edge> es, int s, int vl, int el) {
-        for (int i = 0; i <= vl; i++) {
-            d[i] = 0;
-        }
+    public static boolean negativeLoop(long[] d, List<Edge> es, int s, int vl, int el) {
+        boolean[] negative = new boolean[vl + 1];
         for (int i = 0; i < vl; i++) {
             for (int j = 0; j < el; j++) {
                 Edge e = es.get(j);
                 // System.out.println(e);
+                // System.out.printf("d[%d] = %d d[%d] = %d e.cost = %d%n", e.to, d[e.to], e.from,
+                //    d[e.from], e.cost);
+                if (d[e.from] == INF) {
+                    continue;
+                }
                 if (d[e.to] > d[e.from] + e.cost) {
                     d[e.to] = d[e.from] + e.cost;
-                    if (i == vl - 1) {
-                        return true;
-                    }
+                    // if (i == vl - 1) {
+                    //    return true;
+                    //}
+                    negative[e.to] = true;
+                }
+                // System.out.printf("negative[%d] = %s %n", e.to, negative[e.to]);
+                if (negative[e.from]) {
+                    negative[e.to] = true;
                 }
             }
         }
-        return false;
+        // for (int i = 0; i <= vl; i++) {
+        //    System.out.println(d[i]);
+        //}
+        return negative[vl];
     }
 }
 class Edge {
