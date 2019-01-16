f42a5ca533c66d6e724ad7ee87266704bc19e115
wdkkou <koukouki7547@gmail.com>
2019-01-16 19:41:35 +0900

    ABC060/D

--- AtCorder/ABC/51-60/ABC060/D/Main.java
+++ AtCorder/ABC/51-60/ABC060/D/Main.java
@@ -2,78 +2,46 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int w = sc.nextInt();
-        Map<Long, List<Long>> map = new HashMap<>();
-        List<Long> wList = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            long weight = sc.nextLong();
-            long value = sc.nextLong();
-            if (!map.containsKey(weight)) {
-                map.put(weight, new ArrayList<>());
-            }
-            if (i == 0) {
-                wList.add(weight);
-            }
-            map.get(weight).add(value);
+        long n = sc.nextLong();
+        long s = sc.nextLong();
+        if (s == n) {
+            System.out.println(n + 1);
+            return;
         }
-
-        List<Integer> wCnt = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            long x = wList.get(0) + i;
-            if (!map.containsKey(x)) {
-                wCnt.add(0);
+        for (long b = 2; b * b <= n; b++) {
+            if (solve(n, s, b)) {
+                System.out.println(b);
+                return;
+            }
+        }
+        boolean ok = false;
+        long ans = 1000000000000L;
+        for (long p = 1; p * p < n; p++) {
+            long b = (n - s) / p + 1;
+            // System.out.println("b = " + b);
+            if (b < 2) {
                 continue;
             }
-            wCnt.add(map.get(x).size());
-        }
-
-        for (int i = 1; i < 4; i++) {
-            wList.add(wList.get(0) + i);
-        }
-
-        // System.out.println(map);
-        // System.out.println(wList);
-        // System.out.println(wCnt);
-        for (Map.Entry<Long, List<Long>> entry : map.entrySet()) {
-            Collections.sort(entry.getValue(), Collections.reverseOrder());
-        }
-        //System.out.println(map);
-
-        long ans = 0;
-        for (int w1 = 0; w1 <= wCnt.get(0); w1++) {
-            for (int w2 = 0; w2 <= wCnt.get(1); w2++) {
-                for (int w3 = 0; w3 <= wCnt.get(2); w3++) {
-                    for (int w4 = 0; w4 <= wCnt.get(3); w4++) {
-                        // System.out.printf("w1 %d w2 %d w3 %d w4 %d %n", w1, w2, w3, w4);
-                        long wsum = w1 * wList.get(0) + w2 * wList.get(1) + w3 * wList.get(2)
-                            + w4 * wList.get(3);
-                        if (wsum > w) {
-                            continue;
-                        }
-                        ans = Math.max(ans, solve(map, wList, w1, w2, w3, w4));
-                    }
-                }
+            if (solve(n, s, b)) {
+                ans = Math.min(ans, b);
+                ok = true;
             }
+        }
+        if (!ok) {
+            System.out.println(-1);
+            return;
         }
         System.out.println(ans);
     }
-    public static long solve(
-        Map<Long, List<Long>> map, List<Long> wList, int w1, int w2, int w3, int w4) {
+    public static boolean solve(long n, long s, long num) {
         long ans = 0;
-
-        for (int i = 1; i <= w1; i++) {
-            ans += map.get(wList.get(0)).get(i - 1);
+        while (n > 0) {
+            ans += n % num;
+            n /= num;
         }
-        for (int i = 1; i <= w2; i++) {
-            ans += map.get(wList.get(1)).get(i - 1);
+        if (ans == s) {
+            return true;
         }
-        for (int i = 1; i <= w3; i++) {
-            ans += map.get(wList.get(2)).get(i - 1);
-        }
-        for (int i = 1; i <= w4; i++) {
-            ans += map.get(wList.get(3)).get(i - 1);
-        }
-        return ans;
+        return false;
     }
 }
