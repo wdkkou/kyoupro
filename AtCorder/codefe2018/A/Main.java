40202f493d3f7ee8d723fbfcc4bdc90dd6136b35
wdkkou <koukouki7547@gmail.com>
2019-01-23 17:00:14 +0900

    A/Main.java

---
+++ AtCorder/codefe2018/A/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] c = new int[m][m];
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int l = sc.nextInt();
+            c[a][b] = l;
+        }
+        for (int i = 0; i < n; i++) {
+            int length = 0;
+            for () {
+                length += c[][];
+            }
+            if () {
+            }
+        }
+    }
+}
