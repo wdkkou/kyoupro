e4fd595f6b01c33f095ba414e86384b421d980e1
wdkkou <koukouki7547@gmail.com>
2019-02-18 16:24:23 +0900

    ddc2017-qual/A B C

---
+++ AtCorder/ddcc2017-qual/C/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = sc.nextInt();
+        List<Integer> l = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            l.add(sc.nextInt());
+        }
+        Collections.sort(l);
+
+        int cnt = 0;
+        int j = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int k = l.get(i) + l.get(j);
+            if (j >= i) {
+                break;
+            }
+            if (k <= c - 1) {
+                j++;
+                cnt++;
+                continue;
+            }
+        }
+        int ans = n - cnt;
+        System.out.println(ans);
+    }
+}
