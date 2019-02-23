82a1bd630e467c08ae070d17132192586c1c368e
wdkkou <koukouki7547@gmail.com>
2019-02-24 00:14:50 +0900

    sasao tokei

--- sasao/1004/D/Main.java
+++ sasao/1004/D/Main.java
@@ -4,12 +4,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         double n = sc.nextDouble();
         double m = sc.nextDouble();
-        double x = 30.0000 * n + 0.5000 * m;
+        double x = 30.0000 * (n % 12) + 0.5000 * m;
         double y = 6.0000 * m;
-        if (x > 360) {
-            x -= 360;
-        }
         double ans = Math.abs(x - y);
+        if (ans > 180) {
+            ans = 360 - ans;
+        }
         System.out.printf("%.4f%n", ans);
     }
 }
