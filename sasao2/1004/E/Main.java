403447bed280ffda50b982d344606c3703f1a968
wdkkou <koukouki7547@gmail.com>
2019-02-23 23:17:04 +0900

    change

--- sasao2/1004/E/Main.java
+++ sasao2/1004/E/Main.java
@@ -7,23 +7,7 @@ public class Main {
         int c = sc.nextInt();
         int d = sc.nextInt();
         int e = sc.nextInt();
-        Set<Integer> ans = new TreeSet<>();
-        ans.add(a + b + c);
-        ans.add(a + b + d);
-        ans.add(a + b + e);
-        ans.add(a + c + d);
-        ans.add(a + c + e);
-        ans.add(a + d + e);
-        ans.add(b + c + d);
-        ans.add(b + c + e);
-        ans.add(b + d + e);
-        ans.add(c + d + e);
-        int cnt = 1;
-        for (Integer x : ans) {
-            if (cnt == ans.size() - 2) {
-                System.out.println(x);
-            }
-            cnt++;
-        }
+        int ans = Math.max(a + d + e, b + c + e);
+        System.out.println(ans);
     }
 }
