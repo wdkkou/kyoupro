05dcd6b4742858961465491f0d34fea241355877
wdkkou <koukouki7547@gmail.com>
2019-03-21 01:02:21 +0900

    ABC119 D

--- AtCorder/ABC/111-120/ABC119/D/Main.java
+++ AtCorder/ABC/111-120/ABC119/D/Main.java
@@ -1,45 +1,71 @@
-import java.util.Scanner;
 import java.util.*;
 public class Main {
-    static int[] f = {0, 2, 5, 5, 4, 5, 6, 3, 7, 6};
-    static int INF = Integer.MAX_VALUE;
+    static long INF = 1000000000000L;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        List<Integer> a = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            a.add(sc.nextInt());
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int q = sc.nextInt();
+        long[] s = new long[a + 2];
+        long[] t = new long[b + 2];
+        long[] x = new long[q + 2];
+        s[0] = -INF;
+        for (int i = 1; i <= a; i++) {
+            s[i] = sc.nextLong();
         }
-        Collections.sort(a);
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -INF);
-        dp[0] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (i - f[a.get(j)] < 0) {
-                    continue;
-                }
-                dp[i] = Math.max(dp[i - f[a.get(j)]] + 1, dp[i]);
-            }
-        }
+        s[a + 1] = INF;
 
-        int remain = dp[n];
-        int match = n;
-        String ans = "";
-        while (match > 0) {
-            for (int i = m - 1; i >= 0; i--) {
-                if (match - f[a.get(i)] < 0) {
-                    continue;
-                }
-                if (dp[match - f[a.get(i)]] == remain - 1) {
-                    remain--;
-                    match -= f[a.get(i)];
-                    ans += String.valueOf(a.get(i));
-                    break;
-                }
+        t[0] = -INF;
+        for (int i = 1; i <= b; i++) {
+            t[i] = sc.nextLong();
+        }
+        t[b + 1] = INF;
+
+        for (int i = 0; i < q; i++) {
+            x[i] = sc.nextLong();
+        }
+        Arrays.sort(s);
+        Arrays.sort(t);
+        for (int i = 0; i < q; i++) {
+            int ok = binSearch(s, a + 1, -1, x[i]);
+            long sb = s[ok];
+            long sa = s[ok - 1];
+
+            ok = binSearch(t, b + 1, -1, x[i]);
+            long tb = t[ok];
+            long ta = t[ok - 1];
+
+            long res1 = Math.abs(tb - x[i]) * 2 + Math.abs(sa - x[i]);
+            long res2 = Math.abs(sa - x[i]) * 2 + Math.abs(tb - x[i]);
+            long res3 = Math.abs(ta - x[i]) * 2 + Math.abs(sb - x[i]);
+            long res4 = Math.abs(sb - x[i]) * 2 + Math.abs(ta - x[i]);
+            long res5 = Math.abs(ta - x[i]) + Math.abs(sa - ta);
+            long res6 = Math.abs(sa - x[i]) + Math.abs(ta - sa);
+            long res7 = Math.abs(tb - x[i]) + Math.abs(sb - tb);
+            long res8 = Math.abs(sb - x[i]) + Math.abs(tb - sb);
+            long ans = Math.min(res1,
+                Math.min(res2,
+                    Math.min(res3,
+                        Math.min(res4, Math.min(res5, Math.min(res6, Math.min(res7, res8)))))));
+            System.out.println(ans);
+        }
+    }
+    public static int binSearch(long[] s, long ok, long ng, long x) {
+        while (Math.abs(ok - ng) > 1) {
+            long mid = (ok + ng) / 2;
+            if (solve(s[(int) mid], x)) {
+                ok = mid;
+            } else {
+                ng = mid;
             }
         }
-        System.out.println(ans);
+        return (int) ok;
+    }
+
+    public static boolean solve(long s, long x) {
+        if (s > x) {
+            return true;
+        }
+        return false;
     }
 }
