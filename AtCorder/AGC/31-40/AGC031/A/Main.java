0601c2e25a1dacdbcec528947172622e28f11180
wdkkou <koukouki7547@gmail.com>
2019-03-29 13:38:07 +0900

    abc115 D agc031 A

--- AtCorder/AGC/31-40/AGC031/A/Main.java
+++ AtCorder/AGC/31-40/AGC031/A/Main.java
@@ -1,31 +1,24 @@
 import java.util.*;
 public class Main {
-    static long MOD = 1000000007;
+    static long INF = 1000000007;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String s = sc.next();
-        long ans = n;
+        Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            if(s.charAt(i)){
+            char ch = s.charAt(i);
+            if (!map.containsKey(ch)) {
+                map.put(ch, 0);
             }
+            map.put(ch, map.get(ch) + 1);
         }
+        long ans = 1;
+        for (char key : map.keySet()) {
+            ans *= map.get(key) + 1;
+            ans %= INF;
+        }
+        ans--;
         System.out.println(ans);
-    }
-    public static boolean solve(String b) {
-        boolean ok = true;
-        if (b.length() == 1) {
-            return true;
-        }
-        List<Character> list = new ArrayList<>();
-        list.add(b.charAt(0));
-        for (int i = 1; i < b.length(); i++) {
-            if (list.contains(b.charAt(i))) {
-                ok = false;
-                break;
-            }
-            list.add(b.charAt(i));
-        }
-        return ok;
     }
 }
