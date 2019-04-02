4b7411dc5c3a951e3353a94c89bf1bac815610c7
wdkkou <koukouki7547@gmail.com>
2019-04-02 21:25:32 +0900

    abc098 d

--- AtCorder/ABC/91-100/ABC098/D/Main.java
+++ AtCorder/ABC/91-100/ABC098/D/Main.java
@@ -3,6 +3,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] a = new int[n];
+        long[] a = new long[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextLong();
+        }
+        long ans = syakutori(a, n);
+        System.out.println(ans);
+    }
+    public static long syakutori(long[] a, int n) {
+        int right = 0;
+        long res = 0;
+        long sum = 0;
+        for (int left = 0; left < n; left++) {
+            while (right < n && (sum ^ a[right]) == sum + a[right]) {
+                sum += a[right];
+                right++;
+            }
+            res += Math.max(0, right - left);
+            sum -= a[left];
+        }
+        return res;
     }
 }
