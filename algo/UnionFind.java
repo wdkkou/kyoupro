984bf05bcdb781405b1c0d6ebee16a8be2f34b39
wdkkou <koukouki7547@gmail.com>
2019-04-09 16:14:34 +0900

    unionfind modified

--- algo/UnionFind.java
+++ algo/UnionFind.java
@@ -1,30 +1,5 @@
 import java.util.*;
 public class UnionFind {
-    static int[] par;
-    public UnionFind(int n) {
-        this.par = new int[n];
-        for (int i = 0; i < n; i++) {
-            par[i] = i;
-        }
-    }
-
-    public int root(int x) {
-        if (par[x] == x) {
-            return x;
-        }
-        return par[x] = root(par[x]);
-    }
-    public void unite(int x, int y) {
-        int rx = root(x);
-        int ry = root(y);
-        if (rx == ry) {
-            return;
-        }
-        par[rx] = ry;
-    }
-    public boolean same(int x, int y) {
-        return root(x) == root(y);
-    }
     public static void main(String[] args) {
         int n = 3;
         UnionFind u = new UnionFind(3);
@@ -32,5 +7,56 @@ public class UnionFind {
         for (int i = 0; i < n; i++) {
             System.out.println(u.root(i));
         }
+    }
+    static int[] par;
+    static int[] rank;
+    static int[] size;
+    public UnionFind(int n) {
+        this.par = new int[n];
+        this.rank = new int[n];
+        this.size = new int[n];
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
+            rank[i] = 0;
+            size[i] = 1;
+        }
+    }
+
+    // find == root
+    public int root(int x) {
+        if (par[x] == x) {
+            return x;
+        }
+        return par[x] = root(par[x]);
+    }
+
+    public int getSize(int x) {
+        return size[root(x)];
+    }
+
+    public void unite(int x, int y) {
+        x = root(x);
+        y = root(y);
+        if (x == y) {
+            return;
+        }
+        if (rank[x] < rank[y]) {
+            par[x] = y;
+            size[y] += size[x];
+        } else {
+            par[y] = x;
+            size[x] += size[y];
+            if (rank[x] == rank[y]) {
+                rank[x]++;
+            }
+        }
+    }
+
+    public boolean same(int x, int y) {
+        return root(x) == root(y);
+    }
+
+    public String toString() {
+        return String.format("par = %s %n", Arrays.toString(par));
     }
 }
