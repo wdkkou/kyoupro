aa294c8bd608e9d7ca324a3b0697f7a1ac5ad38f
wdkkou <koukouki7547@gmail.com>
2019-07-02 15:48:17 +0900

    codefe2017 b

---
+++ AtCorder/codefe2017/B/Main.java
@@ -0,0 +1,119 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        String s = sc.next();
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('a', 0);
+        map.put('b', 0);
+        map.put('c', 0);
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.get(ch) + 1);
+        }
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (char key : map.keySet()) {
+            int x = map.get(key);
+            min = Math.min(min, x);
+            max = Math.max(max, x);
+        }
+        if (Math.abs(max - min) <= 1) {
+            System.out.println("YES");
+            return;
+        }
+        System.out.println("NO");
+    }
+}
+
+class FastScanner {
+    private final InputStream in = System.in;
+    private final byte[] buffer = new byte[1024];
+    private int ptr = 0;
+    private int buflen = 0;
+
+    private boolean hasNextByte() {
+        if (ptr < buflen) {
+            return true;
+        } else {
+            ptr = 0;
+            try {
+                buflen = in.read(buffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (buflen <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int readByte() {
+        if (hasNextByte())
+            return buffer[ptr++];
+        else
+            return -1;
+    }
+
+    private static boolean isPrintableChar(int c) {
+        return 33 <= c && c <= 126;
+    }
+
+    public boolean hasNext() {
+        while (hasNextByte() && !isPrintableChar(buffer[ptr]))
+            ptr++;
+        return hasNextByte();
+    }
+
+    public String next() {
+        if (!hasNext())
+            throw new NoSuchElementException();
+        StringBuilder sb = new StringBuilder();
+        int b = readByte();
+        while (isPrintableChar(b)) {
+            sb.appendCodePoint(b);
+            b = readByte();
+        }
+        return sb.toString();
+    }
+
+    public long nextLong() {
+        if (!hasNext())
+            throw new NoSuchElementException();
+        long n = 0;
+        boolean minus = false;
+        int b = readByte();
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+        if (b < '0' || '9' < b) {
+            throw new NumberFormatException();
+        }
+        while (true) {
+            if ('0' <= b && b <= '9') {
+                n *= 10;
+                n += b - '0';
+            } else if (b == -1 || !isPrintableChar(b)) {
+                return minus ? -n : n;
+            } else {
+                throw new NumberFormatException();
+            }
+            b = readByte();
+        }
+    }
+
+    public int nextInt() {
+        long nl = nextLong();
+        if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE)
+            throw new NumberFormatException();
+        return (int) nl;
+    }
+
+    public double nextDouble() {
+        return Double.parseDouble(next());
+    }
+}
